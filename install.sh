917ff7fa0af6c1e82fe2f82c9dc950fd6ae8dc20
Roy Vanegas <roy@thecodeeducators.com>
2018-12-26 16:37:55 -0500

    Add routine to install .bash_aliases

--- install.sh
+++ install.sh
@@ -281,6 +281,22 @@ mv .stylelintrc.json $HOME/
 inform ".stylelintrc.json downloaded and installed to $HOME/.stylelintrc.json"
 
 #####################################################################################
+# Install .bash_aliases file.
+#####################################################################################
+if [ -e ${HOME}/.bash_aliases ]; then
+   pause_and_warn ".bash_aliases file exists. Renaming to .backup.bash_aliases" true
+   mv ${HOME}/.bash_aliases ${HOME}/.backup.bash_aliases
+fi
+
+inform "Downloading .bash_aliases..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/.bash_aliases
+
+inform "Installing .bash_aliases..."
+mv .bash_aliases $HOME/
+
+inform ".bash_aliases downloaded and installed to $HOME/.bash_aliases"
+
+#####################################################################################
 # Install .bashrc file.
 #####################################################################################
 if [ -e ${HOME}/.bashrc ]; then
