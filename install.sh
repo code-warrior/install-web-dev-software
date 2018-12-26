8684daebf2cc44f3c050fadab25268a20c167e49
Roy Vanegas <roy@thecodeeducators.com>
2018-12-26 11:55:49 -0500

    Add routine to install the TomorrowNight Terminal theme file

--- install.sh
+++ install.sh
@@ -269,3 +269,17 @@ inform "Installing .stylelintrc.json..."
 mv .stylelintrc.json $HOME/
 
 inform ".stylelintrc.json downloaded and installed to $HOME/.stylelintrc.json"
+
+#
+# Install Tomorrow Night Terminal theme.
+#
+inform "Downloading TomorrowNight.terminal..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/TomorrowNight.terminal
+
+open TomorrowNight.terminal
+
+inform "Bring up The Terminal’s preferences by typing ⌘ + ,. Choose Profiles, " true
+inform "which is the second tab on the top, then scroll all the way down along the "
+inform "left column until you find TomorrowNight. Click Default, then close the "
+inform "Profiles page."
+pause_and_warn "Once the theme is installed, come back to this Terminal window."
