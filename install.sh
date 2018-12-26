d83fe98d6b349365e5dc79ffb049940b4fa8ee1a
Roy Vanegas <roy@thecodeeducators.com>
2018-12-26 10:44:56 -0500

    Add routine to install .editorconfig file

--- install.sh
+++ install.sh
@@ -237,3 +237,19 @@ else
    rm -fr Ubuntu_Mono
    rm -fr Ubuntu_Mono.zip
 fi
+
+#
+# Install .editorconfig file.
+#
+if [ -e ${HOME}/.editorconfig ]; then
+   pause_and_warn ".editorconfig file exists. Renaming to .backup.editorconfig" true
+   mv ${HOME}/.editorconfig ${HOME}/.backup.editorconfig
+fi
+
+inform "Downloading .editorconfig..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/.editorconfig
+
+inform "Installing .editorconfig..."
+mv .editorconfig $HOME/
+
+inform ".editorconfig downloaded and installed to $HOME/.editorconfig"
