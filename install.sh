3c922d42cb7eb605ffd0ed8ed78eefce7dab0e0d
Roy Vanegas <roy@thecodeeducators.com>
2018-12-26 16:41:05 -0500

    Add routine to install .git-prompt.sh

--- install.sh
+++ install.sh
@@ -281,6 +281,22 @@ mv .stylelintrc.json $HOME/
 inform ".stylelintrc.json downloaded and installed to $HOME/.stylelintrc.json"
 
 #####################################################################################
+# Install .git-prompt.sh file.
+#####################################################################################
+if [ -e ${HOME}/.git-prompt.sh ]; then
+   pause_and_warn ".git-prompt.sh file exists. Renaming to .backup.git-prompt.sh" true
+   mv ${HOME}/.git-prompt.sh ${HOME}/.backup.git-prompt.sh
+fi
+
+inform "Downloading .git-prompt.sh..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/git-env-for-mac-and-windows/.git-prompt.sh
+
+inform "Installing .git-prompt.sh..."
+mv .git-prompt.sh $HOME/
+
+inform ".git-prompt.sh downloaded and installed to $HOME/.git-prompt.sh"
+
+#####################################################################################
 # Install .bash_aliases file.
 #####################################################################################
 if [ -e ${HOME}/.bash_aliases ]; then
