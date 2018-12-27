c0e35fe4aedc777dd4978f5d9323fa655784e775
Roy Vanegas <roy@thecodeeducators.com>
2018-12-27 08:47:44 -0500

    Remove unused variables

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-SYSTEM="mac"
-BASH_FILE=".bash_profile"
 MINIMUM_MAC_OS="10.11.0"
 
 # set 256 color profile where possible
@@ -16,29 +14,16 @@ RESET=$(      tput sgr0)
 
 # Foreground color
 BLACK=$(      tput setaf 0)
-RED=$(        tput setaf 1)
-GREEN=$(      tput setaf 2)
-YELLOW=$(     tput setaf 3)
-BLUE=$(       tput setaf 4)
-MAGENTA=$(    tput setaf 5)
-CYAN=$(       tput setaf 6)
 WHITE=$(      tput setaf 7)
 
 # Background color
-BG_BLACK=$(   tput setab 0)
 BG_RED=$(     tput setab 1)
 BG_GREEN=$(   tput setab 2)
 BG_YELLOW=$(  tput setab 3)
-BG_BLUE=$(    tput setab 4)
-BG_MAGENTA=$( tput setab 5)
-BG_CYAN=$(    tput setab 6)
 BG_WHITE=$(   tput setab 7)
 
 # Style
-UNDERLINE=$(  tput smul)
-NOUNDERLINE=$(tput rmul)
 BOLD=$(       tput bold)
-ITALIC=$(     tput sitm)
 
 function show () {
   echo -e "${BG_WHITE}${BLACK}> $* ${RESET}"
