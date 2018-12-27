4b13909e7f000b680d8f507dbd904d867caa97b1
Roy Vanegas <roy@thecodeeducators.com>
2018-12-27 10:03:51 -0500

    Make sure the backslash in input doesn’t act as an escape character

--- install.sh
+++ install.sh
@@ -47,14 +47,14 @@ function fail () {
 function pause_awhile () {
   if [[ $2 ]]; then echo ""; fi
   echo -e "${BG_YELLOW}${BLACK}${BOLD}>>>>  $1 ${RESET}"
-  read -p "${BG_YELLOW}${BLACK}${BOLD}Press <Enter> to continue.${RESET}"
+  read -r -p "${BG_YELLOW}${BLACK}${BOLD}Press <Enter> to continue.${RESET}"
 }
 
 function pause_and_warn () {
   if [[ $2 ]]; then echo ""; fi
   echo -e "${BG_YELLOW}${BLACK}${BOLD}>>>>  $1 ${RESET}"
   echo -e "${BG_YELLOW}${BLACK}${BOLD}>>>> ${RESET}"
-  read -p "${BG_YELLOW}${BLACK}${BOLD}>>>>  Continue? [Yy] ${RESET} " -n 1 -r
+  read -r -p "${BG_YELLOW}${BLACK}${BOLD}>>>>  Continue? [Yy] ${RESET} " -n 1 -r
 
   if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     fail "Exiting..." true
