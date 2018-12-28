f95e829cb494422f9221370e2877dae66a4f2334
Roy Vanegas <roy@thecodeeducators.com>
2018-12-28 15:15:48 -0500

    Add routine that installs .sass-lint.yml file to users’ computer

--- install.sh
+++ install.sh
@@ -287,6 +287,22 @@ mv .stylelintrc.json "$HOME/"
 inform ".stylelintrc.json downloaded and installed to $HOME/.stylelintrc.json"
 
 #####################################################################################
+# Install .sass-lint.yml file.
+#####################################################################################
+if [ -e "$HOME/.sass-lint.yml" ]; then
+   pause_and_warn ".sass-lint.yml file exists. Renaming to .backup.sass-lint.yml" true
+   mv "$HOME/.sass-lint.yml" "$HOME/.backup.sass-lint.yml"
+fi
+
+inform "Downloading .sass-lint.yml..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/sass/.sass-lint.yml
+
+inform "Installing .sass-lint.yml..."
+mv .sass-lint.yml "$HOME/"
+
+inform ".sass-lint.yml downloaded and installed to $HOME/.sass-lint.yml"
+
+#####################################################################################
 # Install .git-prompt.sh file.
 #####################################################################################
 if [ -e "$HOME/.git-prompt.sh" ]; then
