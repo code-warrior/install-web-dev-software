dcddcc34e690d9c47d8a0d5d3fa10660bfd8869c
Roy Vanegas <roy@thecodeeducators.com>
2018-12-30 12:50:49 -0500

    Move into its own shell file the routine that installs .git-prompt.sh

--- install.sh
+++ install.sh
@@ -124,21 +124,8 @@ source ./includes/installer/install-editorconfig.sh
 source ./includes/installer/install-stylelintrc.json.sh
 source ./includes/installer/install-sass-lint.yml.sh
 
-#####################################################################################
-# Install .git-prompt.sh file
-#####################################################################################
-if [ -e "$HOME/.git-prompt.sh" ]; then
-   pause_and_warn ".git-prompt.sh file exists. Renaming to .backup.git-prompt.sh" true
-   mv -v "$HOME/.git-prompt.sh" "$HOME/.backup.git-prompt.sh"
-fi
-
-inform "Downloading .git-prompt.sh..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/git-env-for-mac-and-windows/.git-prompt.sh
-
-inform "Installing .git-prompt.sh..."
-mv -v .git-prompt.sh "$HOME/"
-
-inform ".git-prompt.sh downloaded and installed to $HOME/.git-prompt.sh"
+# Install Terminal environment
+source ./includes/installer/install-git-prompt.sh
 
 #####################################################################################
 # Install .git-completion.sh file
