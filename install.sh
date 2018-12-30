2d85d518acae1ad07ff7c0c93c8445deece3f537
Roy Vanegas <roy@thecodeeducators.com>
2018-12-30 12:42:55 -0500

    Move routine that installs Ubuntu Mono to its own shell file

--- install.sh
+++ install.sh
@@ -116,29 +116,7 @@ sudo defaults write com.apple.terminal StringEncodings -array 4
 
 # Install typefaces
 source ./includes/installer/install-font--ibm-plex-mono.sh
-
-#####################################################################################
-# Install the Ubunutu Mono typeface
-#####################################################################################
-if [ -e "$HOME/Library/Fonts/UbuntuMono-Regular.ttf" ]; then
-   inform "Ubuntu Mono Regular is already installed." true
-   pause_and_warn
-else
-   inform "Downloading the Ubuntu Mono typeface..."
-   curl "https://fonts.google.com/download?family=Ubuntu%20Mono" -o Ubuntu_Mono.zip
-
-   inform "Unzipping the Ubuntu Mono typeface into Font Book..."
-   unzip Ubuntu_Mono.zip -d Ubuntu_Mono
-
-   inform "Installing the Ubuntu Mono typeface into Font Book..."
-   mv -v Ubuntu_Mono/*.ttf "$HOME/Library/Fonts/"
-
-   inform "Removing Ubuntu_Mono folder..." true
-   rm -fr Ubuntu_Mono
-
-   inform "Removing Ubuntu_Mono.zip file..." true
-   rm -fr Ubuntu_Mono.zip
-fi
+source ./includes/installer/install-font--ubuntu-mono.sh
 
 #####################################################################################
 # Install .editorconfig file
