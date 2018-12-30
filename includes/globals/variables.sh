164898dfc06366c0671dcbf6df69b18cf4bff792
Roy Vanegas <roy@thecodeeducators.com>
2018-12-30 12:38:14 -0500

    Place variables in its own shell file

---
+++ includes/globals/variables.sh
@@ -0,0 +1,28 @@
+#@IgnoreInspection BashAddShebang
+# Reset formatting
+RESET=$(tput sgr0)
+
+# Foreground color
+BLACK=$(tput setaf 0)
+WHITE=$(tput setaf 7)
+
+# Background color
+BG_RED=$(tput setab 1)
+BG_GREEN=$(tput setab 2)
+BG_YELLOW=$(tput setab 3)
+BG_WHITE=$(tput setab 7)
+
+# Style
+BOLD=$(tput bold)
+
+MINIMUM_MAC_OS="10.11.0"
+OS_VERSION=$(sw_vers -productVersion)
+COMP_NAME=$(scutil --get ComputerName)
+LOCL_NAME=$(scutil --get LocalHostName)
+HOST_NAME=$(hostname)
+USER_NAME=$(id -un)
+FULL_NAME=$(finger "$USER_NAME" | awk '/Name:/ {print $4" "$5}')
+USER_GRPS=$(id -Gn "$USER_NAME")
+OS_NUMBER=$(echo "$OS_VERSION" | cut -d "." -f 2)
+MAC_ADDRS=$(ifconfig en0 | grep ether | sed -e 's/^[ \t|ether|\s|\n]*//')
+MAC_ADDRS="$(echo -e "${MAC_ADDRS}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
--- install.sh
+++ install.sh
@@ -7,21 +7,7 @@
 # Set 256 color profile
 export TERM=xterm-256color
 
-# Reset formatting
-RESET=$(tput sgr0)
-
-# Foreground color
-BLACK=$(tput setaf 0)
-WHITE=$(tput setaf 7)
-
-# Background color
-BG_RED=$(tput setab 1)
-BG_GREEN=$(tput setab 2)
-BG_YELLOW=$(tput setab 3)
-BG_WHITE=$(tput setab 7)
-
-# Style
-BOLD=$(tput bold)
+source ./includes/globals/variables.sh
 
 function show () {
   echo -e "${BG_WHITE}${BLACK}> $* ${RESET}"
@@ -86,18 +72,6 @@ inform "necessary changes to your system. For security purposes, Mac OS "
 inform "will not echo to The Terminal the password as you type: "
 
 sudo -p "Password:" echo "${BG_WHITE}> Thank you! ${RESET}"
-
-MINIMUM_MAC_OS="10.11.0"
-OS_VERSION=$(sw_vers -productVersion)
-COMP_NAME=$(scutil --get ComputerName)
-LOCL_NAME=$(scutil --get LocalHostName)
-HOST_NAME=$(hostname)
-USER_NAME=$(id -un)
-FULL_NAME=$(finger "$USER_NAME" | awk '/Name:/ {print $4" "$5}')
-USER_GRPS=$(id -Gn "$USER_NAME")
-OS_NUMBER=$(echo "$OS_VERSION" | cut -d "." -f 2)
-MAC_ADDRS=$(ifconfig en0 | grep ether | sed -e 's/^[ \t|ether|\s|\n]*//')
-MAC_ADDRS="$(echo -e "${MAC_ADDRS}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
 inform "Your current setup is:" true
 inform
