494dcfbaec4f18cb8155c0a476effffdcf32415e
Roy Vanegas <roy@thecodeeducators.com>
2018-12-30 13:20:14 -0500

    Move into its own shell file the routine that installs Typora

--- install.sh
+++ install.sh
@@ -142,33 +142,7 @@ source ./includes/installer/install-terminal-theme--tomorrow-night.sh
 # Install software
 #####################################################################################
 source ./includes/installer/install-spectacle.sh
-
-#####################################################################################
-# Install Typora
-#####################################################################################
-if [ -d "/Applications/Typora.app/" ]; then
-   inform "Typora is already installed on this machine." true
-   pause_and_warn
-else
-   inform "Downloading Typora..."
-   curl -O https://typora.io/download/Typora.dmg
-
-   inform "Installing Typora..."
-   open Typora.dmg
-
-   inform "The Finder should have loaded the Typora image. Copy Typora " true
-   inform "to the Applications folder. Wait until the copy is finished before "
-   inform "proceeding."
-   pause_and_warn
-
-   inform "Ejecting Typora Disc Image..." true
-   diskutil eject /Volumes/Typora/
-
-   inform "Removing Typora Disc Image..." true
-   rm -f Typora.dmg
-
-   inform "Typora’s installer image ejected and its installer removed." true
-fi
+source ./includes/installer/install-typora.sh
 
 #####################################################################################
 # Install GitHub Desktop
