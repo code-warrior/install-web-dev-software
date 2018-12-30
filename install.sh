9379164bc2b8e19be1df05e1f5778a81f355a269
Roy Vanegas <roy@thecodeeducators.com>
2018-12-30 12:48:38 -0500

    Move into its own shell file the routine that installs .stylelintrc.json

--- install.sh
+++ install.sh
@@ -120,21 +120,8 @@ source ./includes/installer/install-font--ubuntu-mono.sh
 
 source ./includes/installer/install-editorconfig.sh
 
-#####################################################################################
-# Install .stylelintrc.json file
-#####################################################################################
-if [ -e "$HOME/.stylelintrc.json" ]; then
-   pause_and_warn ".stylelintrc.json file exists. Renaming to .backup.stylelintrc.json" true
-   mv -v "$HOME/.stylelintrc.json" "$HOME/.backup.stylelintrc.json"
-fi
-
-inform "Downloading .stylelintrc.json..."
-curl -O https://gist.githubusercontent.com/code-warrior/a766f7c32bab9a82b467601800b00a46/raw/768717143df9db9c593dabb38c3c7aa63c87f66b/.stylelintrc.json
-
-inform "Installing .stylelintrc.json..."
-mv -v .stylelintrc.json "$HOME/"
-
-inform ".stylelintrc.json downloaded and installed to $HOME/.stylelintrc.json"
+# Install linters
+source ./includes/installer/install-stylelintrc.json.sh
 
 #####################################################################################
 # Install .sass-lint.yml file
