6458d32e81bcf30114454f4a24046ab277366e4c
Roy Vanegas <roy@thecodeeducators.com>
2018-12-30 13:21:19 -0500

    Move into its own shell file the routine that installs GitHub Desktop

--- install.sh
+++ install.sh
@@ -143,29 +143,7 @@ source ./includes/installer/install-terminal-theme--tomorrow-night.sh
 #####################################################################################
 source ./includes/installer/install-spectacle.sh
 source ./includes/installer/install-typora.sh
-
-#####################################################################################
-# Install GitHub Desktop
-#####################################################################################
-if [ -d "/Applications/GitHub Desktop.app/" ]; then
-   inform "GitHub Desktop is already installed on this machine." true
-   pause_and_warn
-else
-   inform "Downloading GitHub Desktop..."
-   curl 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/GitHubDesktop.zip' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'DNT: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://desktop.github.com/' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o GitHubDesktop.zip
-
-   inform "Unzipping GitHub Desktop... " true
-   inform "Once the installer has been unzipped, move GitHub Desktop into the "
-   inform "Applications folder, then, once it’s copied, return to this script "
-   inform "and continue."
-   open GitHubDesktop.zip
-   pause_and_warn
-
-   inform "Removing GitHubDesktop.zip..." true
-   rm -f GitHubDesktop.zip
-
-   inform "GitHub Desktop’s installer removed." true
-fi
+source ./includes/installer/install-github-desktop.sh
 
 #####################################################################################
 # Install Atom
