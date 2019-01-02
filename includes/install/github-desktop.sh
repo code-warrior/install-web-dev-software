6130f1ba164581ff2ef6ad1768f1c8137fe25b4a
Roy A Vanegas <roy@thecodeeducators.com>
2019-01-02 16:41:17 -0500

    Replace GitHubDesktop.zip with GITHUB_DESKTOP_INSTALLER variable

--- includes/install/github-desktop.sh
+++ includes/install/github-desktop.sh
@@ -6,17 +6,18 @@ if [ -d "/Applications/GitHub Desktop.app/" ]; then
    pause_and_warn
 else
    inform "Downloading GitHub Desktop..."
-   curl 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/GitHubDesktop.zip' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'DNT: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://desktop.github.com/' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o GitHubDesktop.zip
+   curl 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/$GITHUB_DESKTOP_INSTALLER' -H 'Connection:
+   keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'DNT: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://desktop.github.com/' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o "$GITHUB_DESKTOP_INSTALLER"
 
    inform "Unzipping GitHub Desktop... " true
    inform "Once the install has been unzipped, move GitHub Desktop into the "
    inform "Applications folder, then, once it’s copied, return to this script "
    inform "and continue."
-   open GitHubDesktop.zip
+   open "$GITHUB_DESKTOP_INSTALLER"
    pause_and_warn
 
-   inform "Removing GitHubDesktop.zip..." true
-   rm -f GitHubDesktop.zip
+   inform "Removing "$GITHUB_DESKTOP_INSTALLER"..." true
+   rm -f "$GITHUB_DESKTOP_INSTALLER"
 
    inform "GitHub Desktop’s installer removed." true
 fi
