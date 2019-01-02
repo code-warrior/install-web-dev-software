b50d746f998857301727eaef4c59be7eb936516b
Roy A Vanegas <roy@thecodeeducators.com>
2019-01-02 16:24:08 -0500

    Replace .bash_profile with BASH_PROFILE variable

--- includes/install/bash-profile.sh
+++ includes/install/bash-profile.sh
@@ -1,15 +1,15 @@
 #####################################################################################
-# Install .bash_profile file
+# Install $BASH_PROFILE file
 #####################################################################################
-if [ -e "$HOME/.bash_profile" ]; then
-   pause_and_warn ".bash_profile file exists. Renaming to .backup.bash_profile" true
-   mv -v "$HOME/.bash_profile" "$HOME/.backup.bash_profile"
+if [ -e "$HOME/$BASH_PROFILE" ]; then
+   pause_and_warn "$BASH_PROFILE file exists. Renaming to .backup$BASH_PROFILE" true
+   mv -v "$HOME/$BASH_PROFILE" "$HOME/.backup$BASH_PROFILE"
 fi
 
-inform "Downloading .bash_profile..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/.bash_profile
+inform "Downloading $BASH_PROFILE..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/"$BASH_PROFILE"
 
-inform "Installing .bash_profile..."
-mv -v .bash_profile "$HOME/"
+inform "Installing $BASH_PROFILE..."
+mv -v "$BASH_PROFILE" "$HOME/"
 
-inform ".bash_profile downloaded and installed to $HOME/.bash_profile"
+inform "$BASH_PROFILE downloaded and installed to $HOME/$BASH_PROFILE"
