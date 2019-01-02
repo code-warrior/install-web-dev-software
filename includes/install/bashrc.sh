6cc5396d7d0a0afcb18008e6f3d67206f3abcdcc
Roy A Vanegas <roy@thecodeeducators.com>
2019-01-02 16:13:28 -0500

    Replace “.bashrc” with “BASH_RUN_COMMANDS”

--- includes/install/bashrc.sh
+++ includes/install/bashrc.sh
@@ -1,15 +1,23 @@
 #####################################################################################
 # Install .bashrc file
 #####################################################################################
-if [ -e "$HOME/.bashrc" ]; then
-   pause_and_warn ".bashrc file exists. Renaming to .backup.bashrc" true
-   mv -v "$HOME/.bashrc" "$HOME/.backup.bashrc"
+if [ -e "$HOME/$BASH_RUN_COMMANDS" ]; then
+   pause_and_warn "$BASH_RUN_COMMANDS file exists. Renaming to .backup$BASH_RUN_COMMANDS" true
+   mv -v "$HOME/$BASH_RUN_COMMANDS" "$HOME/.backup$BASH_RUN_COMMANDS"
 fi
 
-inform "Downloading .bashrc..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/.bashrc
+inform "Downloading $BASH_RUN_COMMANDS..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/"$BASH_RUN_COMMANDS"
 
-inform "Installing .bashrc..."
-mv -v .bashrc "$HOME/"
+if [ -e "$BASH_RUN_COMMANDS" ]; then
+    inform "$BASH_RUN_COMMANDS downloaded successfully."
 
-inform ".bashrc downloaded and installed to $HOME/.bashrc"
+    inform "Installing $BASH_RUN_COMMANDS..."
+    mv -v "$BASH_RUN_COMMANDS" "$HOME/"
+
+    if [ -e "$BASH_RUN_COMMANDS" ]; then
+        warn "Installing $BASH_RUN_COMMANDS was not successful. Please investigate, the continue."
+        pause_and_warn
+    else
+        inform "The $BASH_RUN_COMMANDS was installed successfully."
+fi
