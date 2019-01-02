8a46233afbb59130bd63f722b7efd567785da1b8
Roy A Vanegas <roy@thecodeeducators.com>
2019-01-02 16:21:09 -0500

    Change ALIASES → BASH_ALIASES

--- includes/install/bash-aliases.sh
+++ includes/install/bash-aliases.sh
@@ -1,21 +1,21 @@
 #####################################################################################
 # Install .bash_aliases file
 #####################################################################################
-if [ -e "$HOME/$ALIASES" ]; then
-   pause_and_warn "$ALIASES file exists. Renaming to .backup$ALIASES" true
-   mv -v "$HOME/$ALIASES" "$HOME/.backup$ALIASES"
+if [ -e "$HOME/$BASH_ALIASES" ]; then
+   pause_and_warn "$BASH_ALIASES file exists. Renaming to .backup$BASH_ALIASES" true
+   mv -v "$HOME/$BASH_ALIASES" "$HOME/.backup$BASH_ALIASES"
 fi
 
-inform "Downloading $ALIASES..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/"$ALIASES"
+inform "Downloading $BASH_ALIASES..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/"$BASH_ALIASES"
 
-inform "Installing $ALIASES..."
-mv -v "$ALIASES" "$HOME/"
+inform "Installing $BASH_ALIASES..."
+mv -v "$BASH_ALIASES" "$HOME/"
 
 # If the file still exists at its current locations, the mv command didn’t work...
-if [ -e "$ALIASES" ]; then
-   warn "Installing $ALIASES was not successful. Please investigate, then continue."
+if [ -e "$BASH_ALIASES" ]; then
+   warn "Installing $BASH_ALIASES was not successful. Please investigate, then continue."
    pause_and_warn
 else
-   inform "The $ALIASES file was installed successfully."
+   inform "The $BASH_ALIASES file was installed successfully."
 fi
