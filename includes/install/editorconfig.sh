4093196fd8c93e7de41d5f85b2740ceb8ad78bb9
Roy Vanegas <roy@thecodeeducators.com>
2019-01-03 10:47:36 -0500

    Convert .editorconfig to a variable: EDITOR_CONFIG_SETTINGS

--- includes/install/editorconfig.sh
+++ includes/install/editorconfig.sh
@@ -1,15 +1,15 @@
 #####################################################################################
 # Install .editorconfig file
 #####################################################################################
-if [ -e "$HOME/.editorconfig" ]; then
-   pause_and_warn ".editorconfig file exists. Renaming to .backup.editorconfig" true
-   mv -v "$HOME/.editorconfig" "$HOME/.backup.editorconfig"
+if [ -e "$HOME/$EDITOR_CONFIG_SETTINGS" ]; then
+   pause_and_warn "$EDITOR_CONFIG_SETTINGS file exists. Renaming to .backup$EDITOR_CONFIG_SETTINGS" true
+   mv -v "$HOME/$EDITOR_CONFIG_SETTINGS" "$HOME/.backup$EDITOR_CONFIG_SETTINGS"
 fi
 
-inform "Downloading .editorconfig..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/.editorconfig
+inform "Downloading $EDITOR_CONFIG_SETTINGS..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/"$EDITOR_CONFIG_SETTINGS"
 
-inform "Installing .editorconfig..."
-mv -v .editorconfig "$HOME/"
+inform "Installing $EDITOR_CONFIG_SETTINGS..."
+mv -v "$EDITOR_CONFIG_SETTINGS" "$HOME/"
 
-inform ".editorconfig downloaded and installed to $HOME/.editorconfig"
+inform "$EDITOR_CONFIG_SETTINGS downloaded and installed to $HOME/$EDITOR_CONFIG_SETTINGS"
