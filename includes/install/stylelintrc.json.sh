4af2b3a065f07d265c6b573d3b651f6e6f27b9d8
Roy Vanegas <roy@thecodeeducators.com>
2019-01-14 09:55:18 -0500

    Replace .stylelintrc.json with STYLELINT_SETTINGS

--- includes/install/stylelintrc.json.sh
+++ includes/install/stylelintrc.json.sh
@@ -1,15 +1,15 @@
 #####################################################################################
 # Install .stylelintrc.json file
 #####################################################################################
-if [[ -e "$HOME/.stylelintrc.json" ]]; then
-   pause_and_warn ".stylelintrc.json file exists. Renaming to .backup.stylelintrc.json" true
-   mv -v "$HOME/.stylelintrc.json" "$HOME/.backup.stylelintrc.json"
+if [[ -e "$HOME/$STYLELINT_SETTINGS" ]]; then
+   pause_and_warn "$STYLELINT_SETTINGS file exists. Renaming to .backup$STYLELINT_SETTINGS" true
+   mv -v "$HOME/$STYLELINT_SETTINGS" "$HOME/.backup$STYLELINT_SETTINGS"
 fi
 
-inform "Downloading .stylelintrc.json..."
-curl -O https://gist.githubusercontent.com/code-warrior/a766f7c32bab9a82b467601800b00a46/raw/768717143df9db9c593dabb38c3c7aa63c87f66b/.stylelintrc.json
+inform "Downloading $STYLELINT_SETTINGS..."
+curl -O https://gist.githubusercontent.com/code-warrior/a766f7c32bab9a82b467601800b00a46/raw/768717143df9db9c593dabb38c3c7aa63c87f66b/"$STYLELINT_SETTINGS"
 
-inform "Installing .stylelintrc.json..."
-mv -v .stylelintrc.json "$HOME/"
+inform "Installing $STYLELINT_SETTINGS..."
+mv -v "$STYLELINT_SETTINGS" "$HOME/"
 
-inform ".stylelintrc.json downloaded and installed to $HOME/.stylelintrc.json"
+inform "$STYLELINT_SETTINGS downloaded and installed to $HOME/$STYLELINT_SETTINGS"
