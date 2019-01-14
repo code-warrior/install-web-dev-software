fc11521124f7e3e09619be9ed04b58cc5fc7a4e1
Roy Vanegas <roy@thecodeeducators.com>
2019-01-14 08:43:05 -0500

    Replace .git-completion.sh with $GIT_COMPLETION

--- includes/install/git-completion.sh
+++ includes/install/git-completion.sh
@@ -1,15 +1,15 @@
 #####################################################################################
 # Install .git-completion.sh file
 #####################################################################################
-if [ -e "$HOME/.git-completion.sh" ]; then
-   pause_and_warn ".git-completion.sh file exists. Renaming to .backup.git-completion.sh" true
-   mv -v "$HOME/.git-completion.sh" "$HOME/.backup.git-completion.sh"
+if [ -e "$HOME/$GIT_COMPLETION" ]; then
+   pause_and_warn "$GIT_COMPLETION file exists. Renaming to .backup$GIT_COMPLETION" true
+   mv -v "$HOME/$GIT_COMPLETION" "$HOME/.backup$GIT_COMPLETION"
 fi
 
-inform "Downloading .git-completion.sh..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/git-env-for-mac-and-windows/.git-completion.sh
+inform "Downloading $GIT_COMPLETION..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/git-env-for-mac-and-windows/"$GIT_COMPLETION"
 
-inform "Installing .git-completion.sh..."
-mv -v .git-completion.sh "$HOME/"
+inform "Installing $GIT_COMPLETION..."
+mv -v "$GIT_COMPLETION" "$HOME/"
 
-inform ".git-completion.sh downloaded and installed to $HOME/.git-completion.sh"
+inform "$GIT_COMPLETION downloaded and installed to $HOME/$GIT_COMPLETION"
