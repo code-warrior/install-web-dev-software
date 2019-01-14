dec42ad3ffed5743b89217f83e8672ffcc7194c1
Roy Vanegas <roy@thecodeeducators.com>
2019-01-14 09:30:59 -0500

    Replace .sass-lint.yml with $SASS_LINT_SETTINGS

--- includes/install/sass-lint.yml.sh
+++ includes/install/sass-lint.yml.sh
@@ -1,15 +1,15 @@
 #####################################################################################
 # Install .sass-lint.yml file
 #####################################################################################
-if [[ -e "$HOME/.sass-lint.yml" ]]; then
-   pause_and_warn ".sass-lint.yml file exists. Renaming to .backup.sass-lint.yml" true
-   mv -v "$HOME/.sass-lint.yml" "$HOME/.backup.sass-lint.yml"
+if [[ -e "$HOME/$SASS_LINT_SETTINGS" ]]; then
+   pause_and_warn "$SASS_LINT_SETTINGS file exists. Renaming to .backup$SASS_LINT_SETTINGS" true
+   mv -v "$HOME/$SASS_LINT_SETTINGS" "$HOME/.backup$SASS_LINT_SETTINGS"
 fi
 
-inform "Downloading .sass-lint.yml..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/sass/.sass-lint.yml
+inform "Downloading $SASS_LINT_SETTINGS..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/sass/"$SASS_LINT_SETTINGS"
 
-inform "Installing .sass-lint.yml..."
-mv -v .sass-lint.yml "$HOME/"
+inform "Installing $SASS_LINT_SETTINGS..."
+mv -v "$SASS_LINT_SETTINGS" "$HOME/"
 
-inform ".sass-lint.yml downloaded and installed to $HOME/.sass-lint.yml"
+inform "$SASS_LINT_SETTINGS downloaded and installed to $HOME/$SASS_LINT_SETTINGS"
