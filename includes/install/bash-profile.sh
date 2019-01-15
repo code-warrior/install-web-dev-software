2da624615501edf99c8bf43393381e8fc06dad14
Roy Vanegas <roy@thecodeeducators.com>
2019-01-15 13:10:53 -0500

    Change BASH_PROFILE to BASH_PFILE to avoid conflict with Bash environment

--- includes/install/bash-profile.sh
+++ includes/install/bash-profile.sh
@@ -3,20 +3,20 @@
 #####################################################################################
 # Install .bash_profile file
 #####################################################################################
-if [[ -e "$HOME/$BASH_PROFILE" ]]; then
-   pause_and_warn "$BASH_PROFILE file exists. Renaming to .backup$BASH_PROFILE" true
-   mv -v "$HOME/$BASH_PROFILE" "$HOME/.backup$BASH_PROFILE"
+if [[ -e "$HOME/$BASH_PFILE" ]]; then
+   pause_and_warn "$BASH_PFILE file exists. Renaming to .backup$BASH_PFILE" true
+   mv -v "$HOME/$BASH_PFILE" "$HOME/.backup$BASH_PFILE"
 fi
 
-inform "Downloading $BASH_PROFILE..."
-curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/"$BASH_PROFILE"
+inform "Downloading $BASH_PFILE..."
+curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/master/terminal/mac/"$BASH_PFILE"
 
-inform "Installing $BASH_PROFILE..."
-mv -v "$BASH_PROFILE" "$HOME/"
+inform "Installing $BASH_PFILE..."
+mv -v "$BASH_PFILE" "$HOME/"
 
-if [[ -e "$BASH_PROFILE" ]]; then
-   warn "$BASH_PROFILE was not successfully installed. Please investigate, then continue."
+if [[ -e "$BASH_PFILE" ]]; then
+   warn "$BASH_PFILE was not successfully installed. Please investigate, then continue."
    pause_and_warn
 else
-   inform "$BASH_PROFILE was installed successfully."
+   inform "$BASH_PFILE was installed successfully."
 fi
