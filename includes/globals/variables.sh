fd95bafee084c72d989fe2ac0ae7139834ca04d7
Roy Vanegas <roy@thecodeeducators.com>
2019-01-15 13:08:21 -0500

    Export variables and wrap values in inch marks

--- includes/globals/variables.sh
+++ includes/globals/variables.sh
@@ -16,17 +16,17 @@ export BG_WHITE="$(tput setab 7)"
 # Style
 export BOLD="$(tput bold)"
 
-MINIMUM_MAC_OS="10.11.0"
-OS_VERSION=$(sw_vers -productVersion)
-COMP_NAME=$(scutil --get ComputerName)
-LOCL_NAME=$(scutil --get LocalHostName)
-HOST_NAME=$(hostname)
-USER_NAME=$(id -un)
-FULL_NAME=$(finger "$USER_NAME" | awk '/Name:/ {print $4" "$5}')
-USER_GRPS=$(id -Gn "$USER_NAME")
-OS_NUMBER=$(echo "$OS_VERSION" | cut -d "." -f 2)
-MAC_ADDRS=$(ifconfig en0 | grep ether | sed -e 's/^[ \t|ether|\s|\n]*//')
-MAC_ADDRS="$(echo -e "${MAC_ADDRS}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+export MINIMUM_MAC_OS="10.11.0"
+export OS_VERSION="$(sw_vers -productVersion)"
+export COMP_NAME="$(scutil --get ComputerName)"
+export LOCL_NAME="$(scutil --get LocalHostName)"
+export HOST_NAME="$(hostname)"
+export USER_NAME="$(id -un)"
+export FULL_NAME="$(finger "$USER_NAME" | awk '/Name:/ {print $4" "$5}')"
+export USER_GRPS="$(id -Gn "$USER_NAME")"
+export OS_NUMBER="$(echo "$OS_VERSION" | cut -d "." -f 2)"
+export MAC_ADDRS="$(ifconfig en0 | grep ether | sed -e 's/^[ \t|ether|\s|\n]*//')"
+export MAC_ADDRS="$(echo -e "${MAC_ADDRS}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
 export ATOM_INSTALLER="atom-mac.zip"
 export ATOM_CONFIG="config.cson"
