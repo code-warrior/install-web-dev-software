207fcc01fddef080e42c68998690ddbf01355e67
Roy Vanegas <roy@thecodeeducators.com>
2019-01-15 00:13:56 -0500

    Update GitHub installer link

--- includes/install/github-desktop.sh
+++ includes/install/github-desktop.sh
@@ -6,8 +6,7 @@ if [[ -d "/Applications/GitHub Desktop.app/" ]]; then
    pause_and_warn
 else
    inform "Downloading GitHub Desktop..."
-   curl 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/$GITHUB_DESKTOP_INSTALLER' -H 'Connection:
-   keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'DNT: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://desktop.github.com/' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o "$GITHUB_DESKTOP_INSTALLER"
+   curl 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/$GITHUB_DESKTOP_INSTALLER' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'DNT: 1' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o "$GITHUB_DESKTOP_INSTALLER"
 
    inform "Unzipping GitHub Desktop... " true
    inform "Once the install has been unzipped, move GitHub Desktop into the "
