d80760d0eded9e4ab99b583f4b1fa9dc57c9c689
Roy Vanegas <roy@thecodeeducators.com>
2019-01-15 11:07:24 -0500

    Update output when installing GitHub Desktop

--- includes/install/github-desktop.sh
+++ includes/install/github-desktop.sh
@@ -9,8 +9,8 @@ else
    curl 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/GitHubDesktop.zip' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'DNT: 1' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o GitHubDesktop.zip
 
    inform "Unzipping GitHub Desktop... " true
-   inform "Once the install has been unzipped, move GitHub Desktop into the "
-   inform "Applications folder, then, once it’s copied, return to this script "
+   inform "Once the installer has been unzipped, move GitHub Desktop into the "
+   inform "Applications folder. When the copy is complete, return to this script "
    inform "and continue."
    open "$GITHUB_DESKTOP_INSTALLER"
    pause_and_warn
