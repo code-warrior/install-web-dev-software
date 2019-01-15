cbd0083aa280721fbc40ebb42356bfbdbb874eed
Roy Vanegas <roy@thecodeeducators.com>
2019-01-15 13:07:57 -0500

    Export variables

--- includes/globals/variables.sh
+++ includes/globals/variables.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Reset formatting
-RESET=$(tput sgr0)
+export RESET="$(tput sgr0)"
 
 # Foreground color
-BLACK=$(tput setaf 0)
-WHITE=$(tput setaf 7)
+export BLACK="$(tput setaf 0)"
+export WHITE="$(tput setaf 7)"
 
 # Background color
-BG_RED=$(tput setab 1)
-BG_GREEN=$(tput setab 2)
-BG_YELLOW=$(tput setab 3)
-BG_WHITE=$(tput setab 7)
+export BG_RED="$(tput setab 1)"
+export BG_GREEN="$(tput setab 2)"
+export BG_YELLOW="$(tput setab 3)"
+export BG_WHITE="$(tput setab 7)"
 
 # Style
-BOLD=$(tput bold)
+export BOLD="$(tput bold)"
 
 MINIMUM_MAC_OS="10.11.0"
 OS_VERSION=$(sw_vers -productVersion)
@@ -28,23 +28,23 @@ OS_NUMBER=$(echo "$OS_VERSION" | cut -d "." -f 2)
 MAC_ADDRS=$(ifconfig en0 | grep ether | sed -e 's/^[ \t|ether|\s|\n]*//')
 MAC_ADDRS="$(echo -e "${MAC_ADDRS}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
-ATOM_INSTALLER="atom-mac.zip"
-ATOM_CONFIG="config.cson"
+export ATOM_INSTALLER="atom-mac.zip"
+export ATOM_CONFIG="config.cson"
 BASH_ALIASES=".bash_aliases"
 BASH_PROFILE=".bash_profile"
-BASH_RUN_COMMANDS=".bashrc"
-EDITOR_CONFIG_SETTINGS=".editorconfig"
-ESLINT_SETTINGS=".eslintrc.json"
-GITHUB_DESKTOP_INSTALLER="GitHubDesktop.zip"
-GIT_PROMPT=".git-prompt.sh"
-GIT_COMPLETION=".git-completion.sh"
-IBM_PLEX_MONO_INSTALLER="IBM_Plex_Mono.zip"
-IBM_PLEX_MONO_FOLDER="IBM_Plex_Mono"
-SASS_LINT_SETTINGS=".sass-lint.yml"
-SPECTACLE_INSTALLER="Spectacle+1.2.zip"
-SPECTACLE_SHORTCUTS_FILE="Shortcuts.json"
-STYLELINT_SETTINGS=".stylelintrc.json"
-TERMINAL_THEME="TomorrowNight.terminal"
-TYPORA_DISK_IMAGE="Typora.dmg"
-UBUNTU_MONO_INSTALLER="Ubuntu_Mono.zip"
-UBUNTU_MONO_FOLDER="Ubuntu_Mono"
+export BASH_RUN_COMMANDS=".bashrc"
+export EDITOR_CONFIG_SETTINGS=".editorconfig"
+export ESLINT_SETTINGS=".eslintrc.json"
+export GITHUB_DESKTOP_INSTALLER="GitHubDesktop.zip"
+export GIT_PROMPT=".git-prompt.sh"
+export GIT_COMPLETION=".git-completion.sh"
+export IBM_PLEX_MONO_INSTALLER="IBM_Plex_Mono.zip"
+export IBM_PLEX_MONO_FOLDER="IBM_Plex_Mono"
+export SASS_LINT_SETTINGS=".sass-lint.yml"
+export SPECTACLE_INSTALLER="Spectacle+1.2.zip"
+export SPECTACLE_SHORTCUTS_FILE="Shortcuts.json"
+export STYLELINT_SETTINGS=".stylelintrc.json"
+export TERMINAL_THEME="TomorrowNight.terminal"
+export TYPORA_DISK_IMAGE="Typora.dmg"
+export UBUNTU_MONO_INSTALLER="Ubuntu_Mono.zip"
+export UBUNTU_MONO_FOLDER="Ubuntu_Mono"
