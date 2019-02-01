dfe25fe34cf2f6e8216ffe38e1ebbd2c0e89030e
Roy Vanegas <roy@thecodeeducators.com>
2019-02-01 16:49:16 -0500

    Add function that installs a config file

--- includes/globals/functions.sh
+++ includes/globals/functions.sh
@@ -54,3 +54,52 @@ function pause_and_warn () {
       exit 1;
    fi
 }
+
+function install_configuration_file() {
+   if [[ -e "$HOME/$1" ]]; then
+      if [[ -e "$HOME/.backup$1" ]]; then
+         inform "$1 and .backup$1 exist." true
+         inform "Nothing to install. Continuing..."
+      else
+         inform "$1 file exists." true
+         pause_and_warn
+
+         inform "Renaming to .backup$1..."
+         mv -v "$HOME/$1" "$HOME/.backup$1"
+
+         inform "Downloading $1..."
+         curl -O "$2"
+
+         if [[ -e "$1" ]]; then
+            inform "$1 downloaded successfully."
+
+            inform "Installing $1..."
+            mv -v "$1" "$HOME/"
+
+            if [[ -e "$1" ]]; then
+               warn "$1 was not successfully installed. Please investigate, then continue."
+               pause_and_warn
+            else
+               inform "$1 was installed successfully."
+            fi
+         fi
+      fi
+   else
+      inform "Downloading $1..."
+      curl -O "$2"
+
+      if [[ -e "$1" ]]; then
+         inform "$1 downloaded successfully."
+
+         inform "Installing $1..."
+         mv -v "$1" "$HOME/"
+
+         if [[ -e "$1" ]]; then
+            warn "$1 was not successfully installed. Please investigate, then continue."
+            pause_and_warn
+         else
+            inform "$1 was installed successfully."
+         fi
+      fi
+   fi
+}
