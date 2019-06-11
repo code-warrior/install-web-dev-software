2f58748cae5bf07c41b0d95a8865c57bb9d727d8
Roy Vanegas <roy@thecodeeducators.com>
2019-06-11 11:04:46 -0400

    Update Atom installation instructions

--- includes/install/atom.sh
+++ includes/install/atom.sh
@@ -11,7 +11,7 @@ else
    curl 'https://atom-installer.github.com/v1.33.1/atom-mac.zip?s=1545258140&ext=.zip' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3657.0 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://atom.io/' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o "$ATOM_INSTALLER"
 
    inform "Unzipping Atom... " true
-   inform "Once the install has been unzipped, move Atom into the Applications "
+   inform "Once the installer has been unzipped, move Atom into the Applications "
    inform "folder and launch it. Then, click Atom in the menu bar on the left, "
    inform "right next to the Apple icon, choose Install Shell Commands, and close"
    inform "Atom once you get confirmation. Return to this script and continue."
