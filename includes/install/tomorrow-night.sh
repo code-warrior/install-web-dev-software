856a283392e5952735a2264034939a5a0407f851
Roy Vanegas <roy@thecodeeducators.com>
2019-06-11 10:37:08 -0400

    Update instructions related to the TomorrowNight theme

--- includes/install/tomorrow-night.sh
+++ includes/install/tomorrow-night.sh
@@ -8,12 +8,13 @@ curl -O https://raw.githubusercontent.com/code-warrior/web-dev-env-config-files/
 
 open "$TERMINAL_THEME"
 
-inform "Bring up The Terminal’s preferences by typing ⌘ + ,. Choose Profiles, " true
-inform "which is the second tab on the top, then scroll all the way down along the "
+inform "Type ⌘ + , in The Terminal to bring up the preferences page. Choose Profiles, " true
+inform "which is the second tab on the top, then scroll all the way down, along the "
 inform "left column until you find TomorrowNight. Click Default, then close the "
 inform "Profiles page."
-
-pause_and_warn "Once the theme is installed, come back to this Terminal window." true
+inform ""
+inform "Once the theme is installed, come back to this Terminal window."
+pause_and_warn
 
 inform "Removing un-needed $TERMINAL_THEME file..."
 rm -f "$TERMINAL_THEME"
