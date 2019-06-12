c34d898157722afb727fc62029b34604a1bcf5dd
Roy Vanegas <roy@thecodeeducators.com>
2019-06-12 09:25:47 -0400

    Update cURL call for GitHub Desktop

--- includes/install/github-desktop.sh
+++ includes/install/github-desktop.sh
@@ -8,7 +8,7 @@ if [[ -d "/Applications/GitHub Desktop.app/" ]]; then
    pause_and_warn
 else
    inform "Downloading GitHub Desktop..."
-   curl 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/GitHubDesktop.zip' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'DNT: 1' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o GitHubDesktop.zip
+   curl 'https://desktop.githubusercontent.com/releases/2.0.3-d79a43a1/GitHubDesktop.zip' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'Referer: https://desktop.githubusercontent.com/' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' --compressed -o GitHubDesktop.zip
 
    inform "Unzipping GitHub Desktop... " true
    open "$GITHUB_DESKTOP_INSTALLER"
