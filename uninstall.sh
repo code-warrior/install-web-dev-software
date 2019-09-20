5b8ac27b9a4a788cc5a795e891ae9f125fd5c07c
Roy A Vanegas <roy@thecodeeducators.com>
2019-09-20 14:36:00 -0400

    Add basic unistall file

---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+sudo rm -f ~/Library/Fonts/IBMPlexMono*.ttf
+rm -f ~/Library/Fonts/UbuntuMono*.ttf
+rm -fr /Applications/FileZilla.app
+rm -fr /Applications/Spectacle.app
+rm -fr /Applications/Typora.app
+rm -fr /Applications/GitHub\ Desktop.app/
+rm -fr /Applications/Atom.app/
+rm -fr /Applications/Slack.app/
+rm -f ~/.stylelintrc.json
+rm -f ~/.backup.stylelintrc.json
+rm -f ~/.git-prompt.sh
+rm -f ~/.backup.git-prompt.sh
+rm -f ~/.git-completion.sh
+rm -f ~/.backup.git-completion.sh
+rm -f ~/.bash_aliases
+rm -f ~/.backup.bash_aliases
+rm -f ~/.bashrc
+rm -f ~/.backup.bashrc
+rm -f ~/.bash_profile
+rm -f ~/.backup.bash_profile
+rm -f ~/.editorconfig
+rm -f ~/.backup.editorconfig
+rm -fr ~/.atom/
+rm -f ~/Library/Application\ Support/Spectacle/Shortcuts.json
