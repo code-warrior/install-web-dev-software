5c8c531718c6d5da7dcbba86fe860f2ad967afce
Roy Vanegas <roy@thecodeeducators.com>
2022-01-27 22:33:43 -0500

    Update FileZilla installation procedure

--- includes/install/filezilla.sh
+++ includes/install/filezilla.sh
@@ -8,29 +8,17 @@ if [[ -d "/Applications/FileZilla.app/" ]]; then
    pause_and_warn
 else
    inform "Downloading FileZilla..."
-   curl -O "https://download.filezilla-project.org/client/$FILEZILLA_INSTALLER"
+   curl 'https://dl2.cdn.filezilla-project.org/client/FileZilla_3.57.0_macosx-x86.app.tar.bz2?h=g_335piQ_OqsU14NmA-nzg&x=1643343357' --compressed -o "$FILEZILLA_INSTALLER"
 
-   inform "Installing FileZilla..."
+   inform "Unzipping Filezilla... " true
+   inform "Once the installer has been unzipped, move FileZilla into the "
+   inform "Applications folder. When the copy is complete, return to this script "
+   inform "and continue."
    open "$FILEZILLA_INSTALLER"
-
-   inform "The Finder should have loaded the GNU General Public License. " true
-   inform "Click “Agree”, then click the FileZilla installer icon."
-   inform "Skip all the ads, then, once the installer is successfully installed,"
-   inform "close FileZilla and come back to this script."
    pause_and_warn
 
-   inform "Ejecting FileZilla disc image..." true
-   diskutil eject /Volumes/FileZilla/ >/dev/null 2>/dev/null
-   if [ $? -eq 0 ]; then
-      inform "Removing FileZilla disc image..." true
-      rm -f "$FILEZILLA_INSTALLER"
+   inform "Removing "$FILEZILLA_INSTALLER"..." true
+   rm -f "$FILEZILLA_INSTALLER"
 
-      if [[ -e "$FILEZILLA_INSTALLER" ]]; then
-         warn "Removing $FILEZILLA_INSTALLER was not successful, Please remove manually." true
-      else
-         inform "FileZilla’s disc image ejected and removed successfully." true
-      fi
-   else
-      fail "The FileZilla disc image could not be found, and thus, nothing was ejected. " true
-   fi
+   inform "FileZilla’s installer removed." true
 fi
